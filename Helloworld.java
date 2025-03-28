606ed671571321d1bdaa89e4bdc36a348e3cbe60
HardikPanchal17 <hardikpanchal60@gmail.com>
2025-03-28 16:15:39 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -4,6 +4,6 @@ class HelloWorld {
     // Prints "Hello, World" to the terminal window.
     public static void main(String[] args)
     {
-        System.out.println("Hello, World this is krisha panchal");
+        System.out.println("Hello, World this is krisha panchal 1");
     }
 }
